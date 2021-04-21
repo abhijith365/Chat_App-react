2d789ecaba6590096264bac252fdb8e942d99d3d
abhijith <abhijithelampal005@gmail.com>
2021-04-21 15:01:27 +0530

    add new src file and create new chat engine config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="
+2ede3048-e0c5-4d5a-80a8-4c2d39a03973"
+            userName=""
+            userSecret=""
+        />
+
+    )
 }
 
 export default App;
