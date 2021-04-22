4a7713ee42e0b3fa0aa983215b6230b504931c32
abhijith <abhijithelampal005@gmail.com>
2021-04-22 14:48:13 +0530

    add chat title container

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -6,7 +6,30 @@ import TheirMessage from 'TheirMessage';
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
 
-    console.log(props)
+    const chat = chats && chats[activeChat];
+
+    const renderMessage = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`mes_${index}`} style={{ width: '100%' }}>
+                    <div className='message-block'>
+                        {
+                            isMyMessage
+                                ? <MyMessage />
+                                : <TheirMessage />
+                        }
+                    </div>
+                </div>
+            )
+        })
+    }
+    renderMessage()
     return (
         <div>
             Chat feed
