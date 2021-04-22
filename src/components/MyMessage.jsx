7cb802ff348eec70ef982b93585d5fe173254188
abhijith <abhijithelampal005@gmail.com>
2021-04-22 16:50:12 +0530

    add their message

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,8 +1,19 @@
 
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if (message?.attachments?.length > 0) {
+        return (
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachments"
+                className="message-image"
+                style={{ float: "right" }}
+            />
+
+        )
+    }
     return (
-        <div>
-            MyMessage
+        <div className="message" style={{ float: "right", marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+            {message.text}
         </div>
     );
 }
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,8 +1,30 @@
 
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>
-            Their Message
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            { message?.attachments?.length > 0
+                ? (
+                    <img
+                        src={message.attachments[0].file}
+                        alt="message-attachments"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+
+                ) : (
+                    <div className="message" style={{ float: "left", color: 'white', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )
+            }
+
         </div>
     );
 }
