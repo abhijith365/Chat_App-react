0450b6d695c248b385514c7d591d11efed7902e1
abhijith <abhijithelampal005@gmail.com>
2021-04-23 13:02:05 +0530

    add input field

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,29 @@
+import { useState } from 'react'
+
 const MessageForm = () => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = () => {
+
+
+    }
+
+    const handleChange = () => {
+
+    }
+
+
+
     return (
-        <div>MessageForm</div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     );
 }
 
