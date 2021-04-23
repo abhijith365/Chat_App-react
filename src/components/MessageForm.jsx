263eedded98404e10e9ed7b791a3c2a2bbe83d3e
abhijith <abhijithelampal005@gmail.com>
2021-04-23 13:23:32 +0530

    add submit button and handleUpload and finish fist layout of messageform

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
     const [value, setValue] = useState('');
@@ -21,6 +22,10 @@ const MessageForm = (props) => {
         isTyping(props, chatId);
     }
 
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.value, text: "" })
+    }
+
 
 
     return (
@@ -32,6 +37,22 @@ const MessageForm = (props) => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
             />
+            <label className="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
+
         </form>
     );
 }
