259a5d5c92fa674735500c6f0c3c6d7fad218fc1
abhijith <abhijithelampal005@gmail.com>
2021-04-23 13:13:14 +0530

    added handleSubmit and handleChange

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
     const [value, setValue] = useState('');
+    const { chatId, creds } = props;
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
+        const text = value.trim()
 
+        if (text.length > 0) sendMessage(creds, chatId, { text });
+
+        setValue('');
     }
 
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setValue(event.target.value);
 
+        isTyping(props, chatId);
     }
 
 
