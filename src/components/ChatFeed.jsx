20daa44799714a0674753a1ee6ec8f6e7d8f9096
abijith365 <abhijithelampal005@gmail.com>
2022-07-30 22:48:46 +0530

    code updated

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,6 +1,6 @@
-import MessageForm from 'MessageForm';
-import MyMessage from 'MyMessage';
-import TheirMessage from 'TheirMessage';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
 
 const ChatFeed = (props) => {
